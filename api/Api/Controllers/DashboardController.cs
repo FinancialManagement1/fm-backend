2755e759c0c27ca74d0c716734c62681835ccfe2
Realgi022 <realgihila@gmail.com>
2026-03-19 19:07:23 +0100

    Configuration of endpoints

---
+++ api/Api/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Api.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("dashboard")]
+    [Authorize]
+    public class DashboardController : ControllerBase
+    {
+        // GET: /dashboard/summary
+        [HttpGet("summary")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponseDto), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ErrorResponseDto), StatusCodes.Status500InternalServerError)]
+        public IActionResult GetDashboardSummary()
+        {
+            return Ok();
+        }
+    }
+}
