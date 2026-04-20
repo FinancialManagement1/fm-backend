c60ee3f2104252a08454e469473fb8b69ed5f58c
Konstantin <konstantindobrev576@gmail.com>
2026-04-20 14:48:52 +0200

    Added extra categories, fixed report summary request

--- api/Api/Controllers/CategoriesController.cs
+++ api/Api/Controllers/CategoriesController.cs
@@ -18,11 +18,23 @@ namespace Api.Controllers
         {
             var items = new List<CategoryItem>
             {
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Food" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Transport" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Shopping" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Bills" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Entertainment" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Health" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Education" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Insurance" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Mortgage" },
+                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Other" },
+
                 new CategoryItem { Type = TransactionType.Income.ToString().ToLowerInvariant(), Name = "Salary" },
                 new CategoryItem { Type = TransactionType.Income.ToString().ToLowerInvariant(), Name = "Freelance" },
-
-                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Food" },
-                new CategoryItem { Type = TransactionType.Expense.ToString().ToLowerInvariant(), Name = "Transport" }
+                new CategoryItem { Type = TransactionType.Income.ToString().ToLowerInvariant(), Name = "Business" },
+                new CategoryItem { Type = TransactionType.Income.ToString().ToLowerInvariant(), Name = "Investment" },
+                new CategoryItem { Type = TransactionType.Income.ToString().ToLowerInvariant(), Name = "Gift" },
+                new CategoryItem { Type = TransactionType.Income.ToString().ToLowerInvariant(), Name = "Other" }
             };
 
             return Ok(new { items });
